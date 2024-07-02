7968ed5c724c555cf2dda03a32ca9a7056ba6f86
Paul Jewell <109604867+pauljewellmsft@users.noreply.github.com>
2024-07-02 16:42:00 -0400

    Progress

---
+++ StorageAccountManagement/ManagementTasks.cs
@@ -0,0 +1,69 @@
+﻿using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage.Models;
+using Azure.ResourceManager.Storage;
+using Azure;
+using Azure.ResourceManager;
+using Azure.Core;
+
+namespace StorageAccountManagement
+{
+    public static class ManagementTasks
+    {
+        public static async Task<StorageAccountResource> CreateStorageAccount(
+            ResourceGroupResource resourceGroup,
+            string storageAccountName)
+        {
+            // Define the settings for the storage account
+            StorageAccountCreateOrUpdateContent parameters = GetStorageAccountParameters();
+
+            // Create a storage account with defined account name and settings
+            StorageAccountCollection accountCollection = resourceGroup.GetStorageAccounts();
+            ArmOperation<StorageAccountResource> acccountCreateOperation = await accountCollection.CreateOrUpdateAsync(WaitUntil.Completed, storageAccountName, parameters);
+            StorageAccountResource storageAccount = acccountCreateOperation.Value;
+
+            return storageAccount;
+        }
+
+        public static StorageAccountCreateOrUpdateContent GetStorageAccountParameters()
+        {
+            AzureLocation location = AzureLocation.WestUS;
+            StorageSku sku = new StorageSku(StorageSkuName.StandardGrs);
+            StorageKind kind = StorageKind.StorageV2;
+            bool allowSharedKeyAccess = false;
+
+            StorageAccountCreateOrUpdateContent parameters = new StorageAccountCreateOrUpdateContent(sku, kind, location);
+
+            return parameters;
+        }
+
+        public static async Task GetStorageAccountsInResourceGroup(ResourceGroupResource resourceGroup)
+        {
+            Console.WriteLine($"List of storage accounts in {resourceGroup.Id.Name}:");
+            await foreach (StorageAccountResource storAcct in resourceGroup.GetStorageAccounts())
+            {
+                Console.WriteLine($"\t{storAcct.Id.Name}");
+            }
+        }
+
+        public static async Task GetStorageAccountsForSubscription(SubscriptionResource subscription)
+        {
+            AsyncPageable<StorageAccountResource> storAcctsSub = subscription.GetStorageAccountsAsync();
+            Console.WriteLine($"List of storage accounts in subscription {subscription.Get().Value.Data.DisplayName}:");
+            await foreach (StorageAccountResource storAcctSub in storAcctsSub)
+            {
+                Console.WriteLine($"\t{storAcctSub.Id.Name}");
+            }
+        }
+
+        public static async Task UpdateStorageAccountSkuAsync(StorageAccountResource storageAccount, StorageAccountCollection accountCollection)
+        {
+            Console.WriteLine("Updating storage account...");
+            // Update storage account sku
+            var currentSku = storageAccount.Get().Value.Data.Sku.Name;  // capture the current Sku value before updating
+            StorageSku updateSku = new StorageSku(StorageSkuName.StandardLrs);
+            StorageAccountCreateOrUpdateContent updateParams = new StorageAccountCreateOrUpdateContent(updateSku, kind, location);
+            await accountCollection.CreateOrUpdateAsync(WaitUntil.Completed, storAccountName, updateParams);
+            Console.WriteLine($"Sku on storage account updated from {currentSku} to {storageAccount.Get().Value.Data.Sku.Name}");
+        }
+    }
+}
--- StorageAccountManagement/Program.cs
+++ StorageAccountManagement/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage;
+using Azure.ResourceManager.Storage.Models;
+
+using StorageAccountManagement;
+
+const string subscriptionId = "<subscription-id>";
+const string rgName = "<resource-group-name>";
+string storageAccountName = "<account-name>";
+
+// Authenticate to Azure and create the top-level ArmClient
+ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+
+// Create a resource identifier and get the subscription resource
+ResourceIdentifier resourceIdentifier = new ResourceIdentifier($"/subscriptions/{subscriptionId}");
+SubscriptionResource subscription = armClient.GetSubscriptionResource(resourceIdentifier);
+
+// Register the Storage resource provider in the subscription
+ResourceProviderResource resourceProvider = await subscription.GetResourceProviderAsync("Microsoft.Storage");
+resourceProvider.Register();
+
+// Create a new resource group (if one already exists then it gets updated)
+ArmOperation<ResourceGroupResource> rgOperation = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, new ResourceGroupData(location));
+ResourceGroupResource resourceGroup = rgOperation.Value;
+Console.WriteLine($"Resource group: {resourceGroup.Id.Name}");
+
+ManagementTasks.CreateStorageAccount(storageAccountName, storageAccountName, storageAccountName,)
+
+// Get all the storage accounts for a given subscription
+await GetStorageAccountsForSubscription(subscription);
+
+// Get a list of storage accounts within a specific resource group
+await GetStorageAccountsInResourceGroup(resourceGroup);
+
+// Get the storage account keys for a given account and resource group
+Pageable<StorageAccountKey> acctKeys = storageAccount.GetKeys();
+
+// Regenerate an account key for a given account
+StorageAccountRegenerateKeyContent regenKeyContent = new StorageAccountRegenerateKeyContent("key1");
+Pageable<StorageAccountKey> regenAcctKeys = storageAccount.RegenerateKey(regenKeyContent);
+
+//Update the storage account for a given account name and resource group
+await UpdateStorageAccountSkuAsync(storageAccount, accountCollection);
+
+// Check if the account name is available
+bool? nameAvailable = subscription.CheckStorageAccountNameAvailability(new StorageAccountNameAvailabilityContent(storAccountName)).Value.IsNameAvailable;
+
+// Delete a storage account with the given account name and a resource group
+storageAccount = await accountCollection.GetAsync(storAccountName);
+Console.WriteLine($"Deleting storage account {storageAccount.Id.Name}");
+await storageAccount.DeleteAsync(WaitUntil.Completed);
+
+Console.ReadLine();
